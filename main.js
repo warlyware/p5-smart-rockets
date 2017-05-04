9bf406977d651bd8fa2b256df569aae007cf5468
Dan Ward <daniel.ward@forrent.com>
2017-05-04 18:02:23 -0400

    Add Rocket function

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ function setup() {
 function draw() {
     background(0);
 }
+
+function Rocket() {
+    this.position = createVector();
+    this.velocity = createVector();
+    this.acceleration = createVector();
+
+    this.applyForce = function(force) {
+        this.acceleration.add(force);
+    }
+
+    this.update = function() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
+    }
+}
